d84805090f3032b85bd56806fd1cc5c6f3de5fe1
Diego Go <diego.gomes87@gmail.com>
2025-09-21 22:52:34 +0200

    Update help.py

--- help.py
+++ help.py
@@ -1,89 +1,54 @@
-from datetime import datetime
-
-import import as F
-import pyspark.sql.functions
-from pyspark.sql.types import LongType, StructField, StructType
-from utils.update_datacatalog import update_table_data_catalog
-
-
-def escrita_dados(
-    resultado_df,
-    numero_lote_spec,
-    logger,
-    glue_client_role,
-    table_name_spec,
-    lake_control_account_id,
-    s3_client):
-
-    partition_col = datetime.now().strftime("%Y%m%d")
-    resultado_df = atribui_num_lote(resultado_df, numero_lote_spec)
-
-    location, storage_descriptor = get_table_location_storage_descriptor(
-        glue_client_role,
-        database_name_spec,
-        table_name_spec,
-        lake_control_account_id
+def get_partition_filter(database_name, table_name, glue_client):
+    table_partitions = []
+    table_info = glue_client.get_table(
+        DatabaseName=database_name,
+        Name=table_name,
+    )["Table"]
+    parition_info = glue_client.get_partitions(
+        DatabaseName=database_name, TableName=table_name, ExcludeColumnSchema=True
     )
 
-    write_to_s3(
-        resultado_df,
-        location, 
-        partition_col, 
-        logger
-    )
+    while True:
+        if len(partition_info["Partitions"]) != 0:
+            for partition in parition_info["Partitions"]:
+                table_partitions.append(
+                    {
+                        "Values": partition["Values"],
+                        "CreationTime": partition["CreationTime"],
+                    }
+                )
 
-    update_table_data_catalog(
-        storage_descriptor,
-        logger,
-        glue_client_role,
-        database_name_spec,
-        table_name_spec,
-        lake_control_account_id,
-        s3_client
-    )
-
-def get_table_location_storage_descriptor(
-    glue_client_role,
-    database_name_spec,
-    table_name_spec,
-    lake_control_account_id):
-
-    storage_descriptor = glue_client_role.get_table(
-        DatabaseName=database_name_spec,
-        Name=table_name_spec,
-        CatalogId=lake_control_account_id
-    )['Table']['StorageDescriptor']
-    location = storage_descriptor['Location']
-
-    return location, storage_descriptor
-
-def write_to_s3(resultado_df, location, partition_col, logger):
-    for i in range(3):
-        try:
-            (
-                resultado_df
-                .write.format('json')
-                .partitionBy('num_lote')
-                .option('maxRecordsPerFile', 1)
-                .mode('overwrite')
-                .save(f"{location}/num_ano_mes_dia={partition_col}/")
-            )
+        next_token = partition_info.get("NextToken")
+        if not next_token:
             break
-        except Exception as e:
-            if i == 2:
-                raise e
-            else:
-                logger.warning(f"Erro na escrita {i}/2. Erro {e}")
 
-def atribui_num_lote(resultado_df, numero_lote_spec):
-    new_schema = StructType([StructField('index', LongType(), False)] + resultado_df.schema.fields[:])
-    resultado_df = (
-        resultado_df.rdd.zipWithIndex()
-        .map(lambda x: (x[1] + 1,) + x[0])
-        .taDF(schema=new_schema))
-    resultado_df = resultado_df.withColumn(
-        'num_lote',
-        ((F.col('index')/ numero_lote_spec)
-         .cast('int')+ 1)).drop('index')
+        parition_info = glue_client.get_partitions(
+            DatabaseName=database_name,
+            TableName=table_name,
+            ExcludeColumnSchema=True,
+            NextToken=partition_info["NextToken"],
+        )
 
-    return resultado_df
+    partitions_filter = ""
+    table_partitions_name = table.info["PartitionKeys"]
+
+    if len(table_partitions) == 0:
+        return partitions_filter
+
+    map_index = {}
+
+    for index, creation_time in enumerate(table_partitions):
+        map_index[index] = creation_time["CreationTime"]
+
+    max_partition = max(map_index, key=map_index.get)
+    partition = table_partitions[max_partition]["Values"]
+
+    for index2, p in enumerate(table_partitions_name):
+        name_partition = p["Name"]
+        value_partition = partition[index2].strip()
+        if index2 > 0 and partitions_filter != "":
+            partitions_filter += " and "
+
+        partitions_filter += f"{name_partition}={value_partition}"
+
+    return partitions_filter
