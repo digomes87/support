f47f1dc3073751079dd29b3d10bc77ffb5cba4e5
Diego Go <diego.gomes87@gmail.com>
2025-09-21 23:56:54 +0200

    Update help.py

--- help.py
+++ help.py
@@ -1,207 +1,132 @@
-import pyspark.sql.functions as F
-from pyspark.sql import DataFrame, Window
-from pyspark.sql.functions import array, col, expr, lit, struct, when
-from pyspark.sql.types import (IntegerType, LongType, StringType, StructField,
-                               StructType)
-from utils.logging_config import ETLLogger
+import copy
+import re
 
-def formata_df(resultado_df: DataFrame, numero_lote_payload_dmp: int, logger: ETLLogger) -> DataFrame:
-    resultado_df = tratamento_df(resultado_df)
-    resultado_df = agrupa_por_id_pessoa(resultado_df)
-    resultado_df = agregacoes_campo(resultado_df)
+import botocore
 
-    if resultado_df is None:
-        logger.error("DataFrame esta none")
-        return None
-    resultado_df = organiza_estruturas_pos_agrupamento(resultado_df)
 
-    resultado_df = compacta_em_lotes(resultado_df, numero_lote_payload_dmp)
-    return resultado_df
+def update_table_data_catalog(date_partition, storage_description, logger, glue_client_role, database_name_spec, table_name_spec, lake_control_account_id, s3_client):
+    logger.info(f'Criando particoes')
+    s3_location = storage_description['Location']
+    bucket_name = s3_location.split('/')[2]  # Extract bucket name from s3://bucket/path
+    s3_prefix = '/'.join(s3_location.split('/')[3:])  # Extract prefix from s3://bucket/path
 
-def organiza_estruturas_pos_agrupamento(resultado_df: DataFrame) -> DataFrame:
-    uuid_expr = F.expr("uuid4()")
-    resultado_df = resultado_df.select(
-            struct(
-                struct(
-                    lit('Decidir análise de crédito PF - Consignado').alias("nom_fncd_serv_nego"),
-                    uuid_expr.alias("cod_idef_prpt_sist_prod"),
-                    uuid_expr.alias("cod_idef_tran_sist_cred"),
-                    uuid_expr.alias("cod_idef_job_jorn"),
-                    uuid_expr.alias("cod_idef_tran_sist_prod"),
-                    lit('Batch').alias("cod_idef_prso_nego"),
-                    col("cod_idef_pess").alias("cod_idef_pess"),
-                    lit('').alias("nom_prso_sist_cred"),
-                    lit('').alias("cod_stat_decs"),
-                    lit('').alias("nom_tipo_decs_prpt"),
-                    lit('').alias("nom_moto_anal_prpt"),
-                    lit('F').alias("cod_tipo_pess"),
-                    lit('').alias("nom_prod_cred"),
-                    lit('').alias("nom_fase_cred"),
-                    lit('').alias("nom_pilo_pltc_cred"),
-                    lit('').alias("cod_vers_pltc"),
-                    lit('').alias("cod_vers_tecn_pltc"),
-                    lit('').alias("dat_hor_exeo_descs"),
-                    ).alias("payloadIdentification"),
-                struct(
-                    lit('Batch').alias("tipoProcesso"),
-                    struct(
-                        lit(3180).alias("codCanal"),
-                        lit(4532).alias("codsubCanal"),
-                        col("numeroConvenio")
-                    ).alias("solicitacao"),
-                    col("dadosOferta"),
-                    col("listaValoresCalculados"),
-                    struct(
-                        col("cod_idef_pess").alias("numeroDocumento"),
-                        col("listaContrato"),
-                        col("dadosEndividamento"),
-                        col("dadosFatorRisco"),
-                        col("indCorrentistaAntigo"),
-                        col("dadosBacen"),
-                        col("dadosPessoaFisica"),
-                        col("dadosVisaoCliente"),
-                        col("dadosModelos"),
-                        col("dadosRenda")
-                    ).alias("proponente")
-                ).alias("payloadInput"),
-                struct(
-                    array().alias('auditSteps'),
-                    array().alias('calculatedVars'),
-                    lit(True).alias('isAuditEnabled'),
-                    lit(True).alias('isCalculatedVarsEnabled'),
-                ).alias('payloadAudit')
-                ).alias('obj_dmp')
-            )
-    return resultado_df
+    limpa_particoes_existentes(database_name_spec, date_partition, glue_client_role, lake_control_account_id, table_name_spec)
+    lista_num_lote = get_lista_num_lote_from_s3(bucket_name, f'{s3_prefix}/num_ano_mes_dia={date_partition}', s3_client)
 
-def agrupa_por_id_pessoa(resultado_df: DataFrame) -> DataFrame:
-    window_spec = Window.partitionBy('cod_idef_pess')
-    resultado_df = resultado_df.withColumn(
-                "valorTotalParcelaInterno",
-                F.sum('Valor_Contratado_Interno').over(window_spec)
-    )
-    resultado_df = resultado_df.withColumn(
-            "calculoDias",
-            col("Situacao_BX").alias("calculoDias")
-    )
+    try:
+        tamanho_lote = 200
+        for i in range(0, len(lista_num_lote), tamanho_lote):
+            list_lote_atual = lista_num_lote[i:i + tamanho_lote]
 
-    resultado_df = resultado_df.withColumn(
-            "dias",
-            when(col("calculoDias").isNull(), lit(-1))
-            .otherwise(col("calculoDias")).cast(IntegerType)
-    )
-    return resultado_df
+            partition_inputs = []
+            for lote in list_lote_atual:
+                custom_storage_description = copy.deepcopy(storage_description)
 
-def agregacoes_campo(resultado_df: DataFrame) -> DataFrame:
-    agregacoes = {
-        "dadosBacen": F.struct(
-           F.first('Modalidade_Operacao', True).alias("codSubModalidadeOperacao"),
-           F.first('tipoInstituicao').alias('tipoInstituicao')
-        ),
-        "dadosModelos": F.struct(
-            F.first('Cross_Consignado', True).alias("crossConsignado"),
-            F.first('Cod_visao_Cliente', True).alias("visaoCliente"),
-            F.first('Gene_Emprego').alias("geneEmprego")
-        ),
-        "dadosFatorRisco": F.struct(
-            F.collect_set(
-                F.col('Codigo_Exclusao_Comercial').alias("codigo")
-            ).alias("listaExclusao"),
-            F.collect_set(
-                F.struct(
-                    F.first('Apontamento_BX').alias("codigo")
+                custom_storage_description['Location'] = storage_description['Location'] + f"/num_ano_mes_dia={date_partition}/num_lote={lote}/"
+                partition_inputs.append(
+                        {
+                            'Values': [
+                                date_partition, lote
+                            ],
+                            "StorageDescriptor": custom_storage_description
+                        }
                 )
-            ).alias("listaApontamentos"),
-            F.first('Gene_Emprego', True).alias("geneEmprego")
-        ),
-        "dadosPessoaFisica": F.struct(
-            F.first('Idade', True).alias("idadeCliente"),
-            F.first('Indicador_SPI', True).alias("spiVisaoConta"),
-            F.first('Indicador_SPO', True).alias("spoVisaoConta"),
-            F.collect_set(F.struct('Vinculo').alias('Vinculo')).alias('listaVinculos'),
-            F.collect_list(
-                when(F.col('Cargos').isNull(), lit(-1)).otherwise(F.col('Cargos')).alias('cargo')
-            ).alias('listaCargos')
-        ),
-        "dadosEndividamento": F.struct(
-                F.first('Parcela_Mercado', True).alias('valorParcConsignado'),
-                F.first('valorTotalParcelaInterno', True).alias('valorParcConsignadoItau')
-        ),
-        "dadosRenda": F.struct(
-                F.first('Renda_Sispag', True).alias('rendaInternaCliente')
-        ),
-        "dadosOferta": F.struct(
-                F.first('Claro_Nao', True).alias('flagInelebilidadeForte')
-        ),
-        "dadosVisaoCliente": F.struct(
-                F.first('Target', True).alias('rating')
-        ),
-        "listaValoresCalculados": F.struct(
-                F.first('pct_maxi_cpmm_rend').alias('percentual'),
-                F.first('qtpzmax').alias('prazo'),
-                F.first('Parcela_Elegivel_Refin').alias('vlrParcelaElegivel')
-        ),
-        "listaApontamentos": F.struct(
-                F.when(F.col('Apontamento_BX').isNull(), -1).otherwise(F.col('Apontamento_BX').cast(IntegerType())).alias('codigo'),
-                F.when(F.col('dias').isNull(), -1).otherwise(F.col('dias')).alias('recencia')
-        ),
-        "indCorrentistaAntigo": F.struct(
-                F.first('indCorrentistaAntigo').alias('indCorrentistaAntigo')
-        ),
-        "listaContratos": F.collect_list(
-                F.struct(
-                    F.first('valorTotalParcelaInterno').alias('valorCnotrato')
-                )
-        ),
-        "numeroConvenio": F.struct(
-                F.first('Numero_Conveio').alias('numeroConvenio')
+            glue_client_role.batch_create_partition(DatabaseName=database_name_spec, TableName=table_name_spec, CatalogId=lake_control_account_id, PartitionInputList=partition_inputs)
+    except botocore.exceptions.ClientError as error:
+        
+        if error.response['Error']['Code'] == 'AlreadyExistsException':
+            logger.warning(f"Particao {date_partition} ja existe")
+        
+        if error.response['Error']['Code'] == 'AccessDeniedException':
+            logger.warning(f"Erro de acesso a tabela: {table_name_spec}, sem permissao ou nao existe")
+
+        if error.response['Error']['Code'] == 'ExpiredTokenException':
+            logger.warning(f"Token de sessions AWS expirado, erro reproduzido ao tentar acessar {table_name_spec}")
+
+        else:
+            logger.error(f"Erro ao tentar gerar particao da tabela {table_name_spec}: {error}")
+            raise error
+
+def limpa_particoes_existentes(database_name_spec, date_partition, glue_client_role, lake_control_account_id, table_name_spec):
+    lista_particoes = get_lista_particoes_spec_from_catalog(date_partition, glue_client_role, database_name_spec, table_name_spec, lake_control_account_id)
+
+    if len(lista_particoes) != 0:
+        delete_batch_spec_partitions(lista_particoes, glue_client_role, lake_control_account_id, database_name_spec, table_name_spec)
+
+def lista_objetos_s3(bucket_name, prefixo, s3_client, continuation_token=None):
+    if continuation_token:
+        return s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefixo, Delimiter='/', ContinuationToken=continuation_token)
+    else:
+        return s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefixo, Delimiter='/')
+
+def extrair_num_lote(pasta, exp_re):
+    match_valor = re.search(exp_re, pasta['Prefix'])
+    if match_valor:
+        return match_valor.group(1)
+    return None
+
+def get_lista_num_lote_from_s3(bucket_name, prefixo, s3_client):
+    pastas = set()
+    continua = True
+    continua_token = None
+    exp_re = r'num_lote=(\d+)'
+
+    while continua:
+        resultado = lista_objetos_s3(bucket_name, prefixo, s3_client, continua_token)
+
+        if 'CommonPrefixes' in resultado:
+            for pasta in resultado['CommonPrefixes']:
+                num_lote = extrair_num_lote(pasta, exp_re)
+                if num_lote:
+                    pastas.add(num_lote)
+        continua_token = resultado.get('NextContinuationToken')
+        continua = continua_token is not None
+
+    list_num_lotes = sorted(pastas, key=int) if pastas else []
+
+    return list_num_lotes
+
+def get_lista_particoes_spec_from_catalog(num_ano_mes_dia, glue_client_role, database_name_spec, table_name_spec, lake_control_account_id):
+    list_particoes = []
+
+    partition_info = glue_client_role.get_partitions(
+            DatabaseName=database_name_spec,
+            TableName=table_name_spec,
+            ExcludeColumnSchema=True,
+            Expression=f"num_ano_mes_dia={num_ano_mes_dia}",
+            CatalogId=lake_control_account_id
+    )
+
+    while True:
+        if partition_info.get('Partitions'):
+            for partition in partition_info['Partitions']:
+                if len(partition['Values']) != 0:
+                    list_particoes.append({'Values': partition['Values']})
+
+        next_token = partition_info.get('NextToken')
+        if not next_token:
+            break
+
+        partition_info = glue_client_role.get_partitions(
+                DatabaseName=database_name_spec,
+                TableName=table_name_spec,
+                ExcludeColumnSchema=True,
+                NextToken=next_token,
+                Expression=f"num_ano_mes_dia={num_ano_mes_dia}",
+                CatalogId=lake_control_account_id
         )
 
-    }
+    return list_particoes
 
-    resultado_agrupado = resultado_df.groupBy('cod_idef_pess').agg(
-            *[agregacoes[key].alias(key) for key in agregacoes]
-    )
+def delete_batch_spec_partitions(lista_particoes, glue_client_role, lake_control_account_id, database_name_spec, table_name_spec):
+    tamanho_lote = 30
+    for i in range(0, len(lista_particoes), tamanho_lote):
+        lote_atual = lista_particoes[i:i + tamanho_lote]
+        glue_client_role.batch_delete_partition(
+            CatalogId=lake_control_account_id,
+            DatabaseName=database_name_spec,
+            TableName=table_name_spec,
+            PartitionsToDelete=lote_atual
+        )
 
-    return resultado_agrupado
 
-
-def tratamento_df(resultado_df):
-    resultado_df = set_IndicadorSPI(resultado_df, 'Indicador_SPI')
-    resultado_df = set_IndicadorSPO(resultado_df, 'Indicador_SPO')
-    resultado_df = set_indicadorCNAO(resultado_df, 'Claro_Nao')
-    resultado_df = calcula_idade(resultado_df)
-    return resultado_df
-
-def calcula_idade(resultado_df):
-    resultado_df = resultado_df.withColumn('Idade', F.to_date(resultado_df['date'], 'yyyy-mm-dd'))
-    resultado_df = resultado_df.withColumn('Idade', F.floor(F.datediff(F.current_date(), F.col('Idade')) / 356.25))
-    return resultado_df
-
-def set_IndicadorSPI(resultado_df, Indicador_SPI):
-    resultado_df = resultado_df.withColumn(
-        'Indicador_SPI',
-        F.when(F.col('Indicador_SPI').isNull(), 'False').otherwise('True')
-    )
-    return resultado_df
-
-def set_IndicadorSPO(resultado_df, Indicador_SPO):
-    resultado_df = resultado_df.withColumn(
-        'Indicador_SPO',
-        F.when(F.col('Indicador_SPO').isNull(), 'False').otherwise('True')
-    )
-    return resultado_df
-
-def set_indicadorCNAO(resultado_df, Claro_Nao):
-    resultado_df = resultado_df.withColumn(
-        'Claro_Nao',
-        F.when(F.col('Claro_Nao') == 0, 'False').otherwise('True')
-    )
-    return resultado_df
-
-def compacta_em_lotes(resultado_df, numero_lote_payload_dmp):
-    new_schema = StructType([StructField('index', LongType(), False)] + resultado_df.schema.fields[:])
-    resultado_df = (resultado_df.rdd.zipWithIndex().map(lambda x : (x[1],) + x[0]).toDF(schema=new_schema))
-    resultado_df = resultado_df.withColumn('num_lote', ((F.col('index')/ numero_lote_payload_dmp).cast('int'))).drop('index')
-    resultado_df = (resultado_df.groupBy('num_lote').agg(F.collect_list('obj_dmp').alias('fixed_array_of_Payload')).drop('num_lote'))
-    return resultado_df
