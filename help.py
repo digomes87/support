f8b7a38a05f161790b99c3fa27f61fe8c43a3803
Diego Go <diego.gomes87@gmail.com>
2025-09-22 11:34:37 +0200

    getPartitons

--- help.py
+++ help.py
@@ -1,199 +1,57 @@
-import pyspark.sql.functions as F
-from pyspark.sql import DataFrame, Window
-from pyspark.sql.functions import array, col, expr, lit, struct, when
-from pyspark.sql.types import (IntegerType, LongType, StringType, StructField,
-                               StructType)
-from utils.logging_config import ETLLogger
+from typing import Any
 
-def formata_df(resultado_df: DataFrame, numero_lote_payload_dmp: int, logger: ETLLogger) -> DataFrame:
-    resultado_df = tratamento_df(resultado_df)
-    resultado_df = agrupa_por_id_pessoa(resultado_df)
-    resultado_df = agregacoes_campo(resultado_df)
-
-    if resultado_df is None:
-        logger.error("DataFrame esta none")
-        return None
-    resultado_df = organiza_estruturas_pos_agrupamento(resultado_df)
-
-    resultado_df = compacta_em_lotes(resultado_df, numero_lote_payload_dmp)
-    return resultado_df
-
-def organiza_estruturas_pos_agrupamento(resultado_df: DataFrame) -> DataFrame:
-    uuid_expr = F.expr("uuid()")
-    resultado_df = resultado_df.select(
-            struct(
-                struct(
-                    lit('Decidir análise de crédito PF - Consignado').alias("nom_fncd_serv_nego"),
-                    uuid_expr.alias("cod_idef_prpt_sist_prod"),
-                    uuid_expr.alias("cod_idef_tran_sist_cred"),
-                    uuid_expr.alias("cod_idef_job_jorn"),
-                    uuid_expr.alias("cod_idef_tran_sist_prod"),
-                    lit('Batch').alias("cod_idef_prso_nego"),
-                    col("cod_idef_pess").alias("cod_idef_pess"),
-                    lit('').alias("nom_prso_sist_cred"),
-                    lit('').alias("cod_stat_decs"),
-                    lit('').alias("nom_tipo_decs_prpt"),
-                    lit('').alias("nom_moto_anal_prpt"),
-                    lit('F').alias("cod_tipo_pess"),
-                    lit('').alias("nom_prod_cred"),
-                    lit('').alias("nom_fase_cred"),
-                    lit('').alias("nom_pilo_pltc_cred"),
-                    lit('').alias("cod_vers_pltc"),
-                    lit('').alias("cod_vers_tecn_pltc"),
-                    lit('').alias("dat_hor_exeo_descs"),
-                    ).alias("payloadIdentification"),
-                struct(
-                    lit('Batch').alias("tipoProcesso"),
-                    struct(
-                        lit(3180).alias("codCanal"),
-                        lit(4532).alias("codsubCanal"),
-                        col("numeroConvenio")
-                    ).alias("solicitacao"),
-                    col("dadosOferta"),
-                    col("listaValoresCalculados"),
-                    struct(
-                        col("cod_idef_pess").alias("numeroDocumento"),
-                        col("listaContratos"),
-                        col("dadosEndividamento"),
-                        col("dadosFatorRisco"),
-                        col("indCorrentistaAntigo"),
-                        col("dadosBacen"),
-                        col("dadosPessoaFisica"),
-                        col("dadosVisaoCliente"),
-                        col("dadosModelos"),
-                        col("dadosRenda")
-                    ).alias("proponente")
-                ).alias("payloadInput"),
-                struct(
-                    array().alias('auditSteps'),
-                    array().alias('calculatedVars'),
-                    lit(True).alias('isAuditEnabled'),
-                    lit(True).alias('isCalculatedVarsEnabled'),
-                ).alias('payloadAudit')
-                ).alias('obj_dmp')
-            )
-    return resultado_df
-
-def agrupa_por_id_pessoa(resultado_df: DataFrame) -> DataFrame:
-    window_spec = Window.partitionBy('cod_idef_pess')
-    resultado_df = resultado_df.withColumn(
-                "valorTotalParcelaInterno",
-                F.sum('Valor_Contratado_Interno').over(window_spec)
-    )
-    resultado_df = resultado_df.withColumn(
-            "calculoDias",
-            col("Situacao_BX").alias("calculoDias")
+def get_partition_filter(database_name: str, table_name: str, glue_client: Any) -> str:
+    table_partitions = []
+    table_info = glue_client.get_table(
+        DatabaseName=database_name,
+        Name=table_name,
+    )["Table"]
+    partition_info = glue_client.get_partitions(
+        DatabaseName=database_name, TableName=table_name, ExcludeColumnSchema=True
     )
 
-    resultado_df = resultado_df.withColumn(
-            "dias",
-            when(col("calculoDias").isNull(), lit(-1))
-            .otherwise(col("calculoDias")).cast(IntegerType())
-    )
-    return resultado_df
-
-def agregacoes_campo(resultado_df: DataFrame) -> DataFrame:
-    agregacoes = {
-        "dadosBacen": F.struct(
-           F.first('Modalidade_Operacao', True).alias("codSubModalidadeOperacao"),
-           F.first('tipoInstituicao').alias('tipoInstituicao')
-        ),
-        "dadosModelos": F.struct(
-            F.first('Cross_Consignado', True).alias("crossConsignado"),
-            F.first('Cod_visao_Cliente', True).alias("visaoCliente"),
-            F.first('Gene_Emprego').alias("geneEmprego")
-        ),
-        "dadosFatorRisco": F.struct(
-            F.collect_set('Codigo_Exclusao_Comercial').alias("listaExclusao"),
-            F.collect_set('Apontamento_BX').alias("listaApontamentos"),
-            F.first('Gene_Emprego', True).alias("geneEmprego")
-        ),
-        "dadosPessoaFisica": F.struct(
-            F.first('Idade', True).alias("idadeCliente"),
-            F.first('Indicador_SPI', True).alias("spiVisaoConta"),
-            F.first('Indicador_SPO', True).alias("spoVisaoConta"),
-            F.collect_set('Vinculo').alias('listaVinculos'),
-            F.collect_list('Cargos').alias('listaCargos')
-        ),
-        "dadosEndividamento": F.struct(
-                F.first('Parcela_Mercado', True).alias('valorParcConsignado'),
-                F.first('valorTotalParcelaInterno', True).alias('valorParcConsignadoItau')
-        ),
-        "dadosRenda": F.struct(
-                F.first('Renda_Sispag', True).alias('rendaInternaCliente')
-        ),
-        "dadosOferta": F.struct(
-                F.first('Claro_Nao', True).alias('flagInelebilidadeForte')
-        ),
-        "dadosVisaoCliente": F.struct(
-                F.first('Target', True).alias('rating')
-        ),
-        "listaValoresCalculados": F.struct(
-                F.first('pct_maxi_cpmm_rend').alias('percentual'),
-                F.first('qtpzmax').alias('prazo'),
-                F.first('Parcela_Elegivel_Refin').alias('vlrParcelaElegivel')
-        ),
-        "listaApontamentos": F.struct(
-                F.first(F.when(F.col('Apontamento_BX').isNull(), -1).otherwise(F.col('Apontamento_BX').cast(IntegerType())), True).alias('codigo'),
-                F.first(F.when(F.col('dias').isNull(), -1).otherwise(F.col('dias')), True).alias('recencia')
-        ),
-        "indCorrentistaAntigo": F.struct(
-                F.first('indCorrentistaAntigo').alias('indCorrentistaAntigo')
-        ),
-        "listaContratos": F.collect_list(
-                F.struct(
-                    F.col('valorTotalParcelaInterno').alias('valorCnotrato')
+    while True:
+        if len(partition_info["Partitions"]) != 0:
+            for partition in partition_info["Partitions"]:
+                table_partitions.append(
+                    {
+                        "Values": partition["Values"],
+                        "CreationTime": partition["CreationTime"],
+                    }
                 )
-        ),
-        "numeroConvenio": F.struct(
-                F.first('Numero_Conveio').alias('numeroConvenio')
+
+        next_token = partition_info.get("NextToken")
+        if not next_token:
+            break
+
+        partition_info = glue_client.get_partitions(
+            DatabaseName=database_name,
+            TableName=table_name,
+            ExcludeColumnSchema=True,
+            NextToken=partition_info["NextToken"],
         )
 
-    }
+    partitions_filter = ""
+    table_partitions_name = table_info["PartitionKeys"]
 
-    resultado_agrupado = resultado_df.groupBy('cod_idef_pess').agg(
-            *[agregacoes[key].alias(key) for key in agregacoes]
-    )
+    if len(table_partitions) == 0:
+        return partitions_filter
 
-    return resultado_agrupado
+    map_index = {}
 
+    for index, creation_time in enumerate(table_partitions):
+        map_index[index] = creation_time["CreationTime"]
 
-def tratamento_df(resultado_df: DataFrame) -> DataFrame:
-    resultado_df = set_IndicadorSPI(resultado_df, 'Indicador_SPI')
-    resultado_df = set_IndicadorSPO(resultado_df, 'Indicador_SPO')
-    resultado_df = set_indicadorCNAO(resultado_df, 'Claro_Nao')
-    resultado_df = calcula_idade(resultado_df)
-    return resultado_df
+    # Find the partition with the latest creation time
+    max_partition = max(map_index, key=lambda x: map_index[x])
+    partition = table_partitions[max_partition]["Values"]
 
-def calcula_idade(resultado_df: DataFrame) -> DataFrame:
-    resultado_df = resultado_df.withColumn('Idade', F.to_date(resultado_df['date'], 'yyyy-mm-dd'))
-    resultado_df = resultado_df.withColumn('Idade', F.floor(F.datediff(F.current_date(), F.col('Idade')) / 356.25))
-    return resultado_df
+    for index2, p in enumerate(table_partitions_name):
+        name_partition = p["Name"]
+        value_partition = partition[index2].strip()
+        if index2 > 0 and partitions_filter != "":
+            partitions_filter += " and "
 
-def set_IndicadorSPI(resultado_df: DataFrame, Indicador_SPI: str) -> DataFrame:
-    resultado_df = resultado_df.withColumn(
-        'Indicador_SPI',
-        F.when(F.col('Indicador_SPI').isNull(), 'False').otherwise('True')
-    )
-    return resultado_df
+        partitions_filter += f"{name_partition}={value_partition}"
 
-def set_IndicadorSPO(resultado_df: DataFrame, Indicador_SPO: str) -> DataFrame:
-    resultado_df = resultado_df.withColumn(
-        'Indicador_SPO',
-        F.when(F.col('Indicador_SPO').isNull(), 'False').otherwise('True')
-    )
-    return resultado_df
-
-def set_indicadorCNAO(resultado_df: DataFrame, Claro_Nao: str) -> DataFrame:
-    resultado_df = resultado_df.withColumn(
-        'Claro_Nao',
-        F.when(F.col('Claro_Nao') == 0, 'False').otherwise('True')
-    )
-    return resultado_df
-
-def compacta_em_lotes(resultado_df: DataFrame, numero_lote_payload_dmp: int) -> DataFrame:
-    new_schema = StructType([StructField('index', LongType(), False)] + resultado_df.schema.fields[:])
-    resultado_df = (resultado_df.rdd.zipWithIndex().map(lambda x : (x[1],) + x[0]).toDF(schema=new_schema))
-    resultado_df = resultado_df.withColumn('num_lote', ((F.col('index')/ numero_lote_payload_dmp).cast('int'))).drop('index')
-    resultado_df = (resultado_df.groupBy('num_lote').agg(F.collect_list('obj_dmp').alias('fixed_array_of_Payload')).drop('num_lote'))
-    return resultado_df
+    return partitions_filter
